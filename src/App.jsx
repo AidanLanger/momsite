b99814f479f6160ca88380efac58ae1f8c3cec23
Aidan Langer <aidan.langer@gmail.com>
2022-06-01 20:59:11 -0230

    first push

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="bg-violet-200 h-full p-9">
+      <div className="flex absolute">
+        <div className="text-xl font-semibold">kara pinksen</div>
+        <div className="flex mx-24 ">
+          <div className="px-16">about</div>
+          <div>contact</div>
+        </div>
+      </div>
+      <div className=" text-lg h-10 w-10 py-16">hero</div>
+      <div className="flex">
+        <div className="">info</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
